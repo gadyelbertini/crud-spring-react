eb58135f8bc9de28f7fd163a662cdbe2af86475e
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 16:28:32 -0300

    Entidade

--- backend/src/main/java/br/com/api/pessoa/model/Pessoa.java
+++ backend/src/main/java/br/com/api/pessoa/model/Pessoa.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,4 +18,8 @@ public class Pessoa {
 	private Integer id;
 	private String nome;
 	private String cidade;
+
+	@Lob
+	private byte[] imagem;
+	private String extensao;
 }
