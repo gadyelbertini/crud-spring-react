c4eef57da47de87919279dc99b881c4810aee288
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 13:45:18 -0300

    Camada de entidade

---
+++ src/main/java/br/com/api/pessoa/model/Pessoa.java
@@ -0,0 +1,13 @@
+package br.com.api.pessoa.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pessoa {
+	
+	private Integer id;
+	private String nome;
+	private String cidade;
+}
