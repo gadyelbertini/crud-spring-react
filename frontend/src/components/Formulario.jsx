973ccac287e32f06835b2dd48422944ed581681f
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 15:03:15 -0300

    Estruturar projeto React

---
+++ frontend/src/components/Formulario.jsx
@@ -0,0 +1,8 @@
+// Componente
+function Formulario(){
+	// Render
+	return <h1>Formulário</h1>;
+}
+
+// Exportar
+export default Formulario;
---
+++ frontend/src/components/Tabela.jsx
@@ -0,0 +1,8 @@
+// Componente
+function Tabela(){
+	// Render
+	return <h1>Tabela</h1>;
+}
+
+// Exportar
+export default Tabela;
