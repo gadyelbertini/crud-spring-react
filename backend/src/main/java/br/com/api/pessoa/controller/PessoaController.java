5ebfee7e0b8202da3572dda3dc8e1bf41f8b5d1e
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 17:08:01 -0300

    Rota de listagem

--- backend/src/main/java/br/com/api/pessoa/controller/PessoaController.java
+++ backend/src/main/java/br/com/api/pessoa/controller/PessoaController.java
@@ -1,9 +1,14 @@
 package br.com.api.pessoa.controller;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.pessoa.model.Pessoa;
 import br.com.api.pessoa.service.PessoaService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin(value = "*")
 @RestController
@@ -14,5 +19,11 @@ public class PessoaController {
 	// Construtor
 	public PessoaController(PessoaService servico){
 		this.servico = servico;
+	}
+
+	// Rota para listar todas as pessoas
+	@GetMapping("/selecionar")
+	public ResponseEntity<Iterable<Pessoa>> selecionar() {
+		return ResponseEntity.ok(servico.selecionar());
 	}
 }
