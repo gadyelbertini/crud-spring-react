67a8a79fa7f95d79d4247cde18f8bcc07b19a242
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 17:05:34 -0300

    Controle

--- backend/src/main/java/br/com/api/pessoa/controller/PessoaController.java
+++ backend/src/main/java/br/com/api/pessoa/controller/PessoaController.java
@@ -1,55 +1,18 @@
 package br.com.api.pessoa.controller;
-
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.api.pessoa.model.Pessoa;
-import br.com.api.pessoa.repository.PessoaRepository;
+import br.com.api.pessoa.service.PessoaService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @CrossOrigin(value = "*")
 @RestController
 public class PessoaController {
+	// Referenciar PassoaServico
+	private final PessoaService servico;
 
-	// Referenciar repositório
-	@Autowired
-	private PessoaRepository repository;
-	
-	// Rota para retornar um Hello World!
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello World!";
-	}
-	
-	// Rota para cadastrar
-	@PostMapping("/cadastrar")
-	public Pessoa cadastrar(@RequestBody Pessoa p) {
-		return repository.save(p);
-	}
-
-	// Rota para selecionar
-	@GetMapping("/selecionar")
-	public Iterable<Pessoa> selecionar(){
-		return repository.findAll();
-	}
-
-	// Rota para alterar
-	@PutMapping("/alterar/{id}")
-	public Pessoa alterar(@PathVariable Integer id, @RequestBody Pessoa p) {
-		p.setId(id);
-		return repository.save(p);
-	}
-
-	// Rota para remover
-	@DeleteMapping("/remover/{id}")
-	public void remover(@PathVariable Integer id){
-		repository.deleteById(id);
+	// Construtor
+	public PessoaController(PessoaService servico){
+		this.servico = servico;
 	}
 }
