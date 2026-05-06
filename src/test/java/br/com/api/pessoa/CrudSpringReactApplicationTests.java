8a5ce3c8f5423b5924b0a58810975709a3ba66d8
gadyelbertini <gadyelbertini@gmail.com>
2026-05-06 13:23:00 -0300

    first commit

---
+++ src/test/java/br/com/api/pessoa/CrudSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
